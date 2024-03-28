b3ff563ce67edc608f8bf0c8d307fb272e943543
HikariDawn777 <boyangwa@umich.edu>
2024-03-27 22:21:54 -0400

    fix: GPU depedency bug

--- test_code/test_utils.py
+++ test_code/test_utils.py
@@ -4,10 +4,8 @@ import torch
 # Import files from same folder
 root_path = os.path.abspath('.')
 sys.path.append(root_path)
-from opt import opt
 from architecture.rrdb import RRDBNet
 from architecture.grl import GRL
-from architecture.swinir import SwinIR
 from architecture.cunet import UNet_Full
 
 
